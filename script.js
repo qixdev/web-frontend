f641c13977ad75a8a2e748b84c266abe3f20f9c5
Danial <230408@astanait.edu.kz>
2024-10-21 14:00:19 +0500

    add product filters

--- script.js
+++ script.js
@@ -37,3 +37,29 @@ function applySavedTheme() {
 
 document.getElementById('theme-toggle').addEventListener('click', toggleTheme);
 window.addEventListener('load', applySavedTheme);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // some explanation for myself
+    const filterButtons = document.querySelectorAll('.filter-btn'); // get the buttons state
+    const productItems = document.querySelectorAll('#product-list .col-lg-4'); // get the items
+
+    filterButtons.forEach(button => {// for each button in buttons
+        button.addEventListener('click', () => { // adding eventListener
+            const filterValue = button.getAttribute('data-filter'); // getting the state of the attribute
+
+            productItems.forEach(item => { // for each product item  in list
+                if (filterValue === 'all') { // if the filter value is all then we get everything, adn display each element
+                    item.style.display = 'block';
+                } else if (item.classList.contains(filterValue)) { // otherwise check if the item class is the filter value
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+
+            filterButtons.forEach(btn => btn.classList.remove('active')); // mark all buttons as inactive
+            button.classList.add('active'); // mark the only clicked button as active.
+        });
+    });
+});
